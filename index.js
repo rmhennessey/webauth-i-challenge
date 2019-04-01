cd4f849faa511b50299f4285b05e4b5a43afeeaf
RMH <rory.hennessey@gmail.com>
2019-04-01 18:02:30 -0400

    mvp completed minus the tasks that we have not yet covered.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.get('/', (req, res) => {
     let user = req.body;
   
     // hash the password
-    const hash = bcrypt.hashSync(user.password, 4);
+    const hash = bcrypt.hashSync(user.password, 8);
     user.password = hash;
   
     Users.add(user)
@@ -51,6 +51,46 @@ server.get('/', (req, res) => {
         res.status(500).json(error);
       });
   });
+
+  server.get('/api/users', restricted, (req, res) => {
+    Users.find()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => res.send(err));
+  });
+  
+//   function only(username) {
+//     return function(req, res, next) {
+//       if (req.headers.username === username) {
+//         next();
+//       } else {
+//         res.status(403).json({ message: `you are not ${username}` });
+//       }
+//     };
+//   }
+  
+  function restricted(req, res, next) {
+    const { username, password } = req.headers;
+  
+    if (username && password) {
+      Users.findBy({ username })
+        .first()
+        .then(user => {
+          // check tha password guess against the database
+          if (user && bcrypt.compareSync(password, user.password)) {
+            next();
+          } else {
+            res.status(401).json({ message: 'You shall not pass!!' });
+          }
+        })
+        .catch(error => {
+          res.status(500).json(error);
+        });
+    } else {
+      res.status(401).json({ message: 'Please provide credentials' });
+    }
+  }
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Welcome to the Jungle ${port} **\n`));
